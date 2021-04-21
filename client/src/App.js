ad6b822bd6d158d3262fa271b62bf0df29b230fd
Michael <memiller219@gmail.com>
2021-04-21 13:13:56 -0700

    added header, footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
-  return <div className="App">skiBay</div>;
-}
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to skiBAY</h1>
+        </Container>
+      </main>
+
+      <Footer />
+    </>
+  );
+};
 
 export default App;
