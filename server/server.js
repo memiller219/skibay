e2d18da2e68e848dcd9b06133bb0c58f113fb07a
Michael <memiller219@gmail.com>
2021-04-23 10:54:30 -0700

    added mongoose, users, products, and order models and schema

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-const express = require("express");
-const dotenv = require('dotenv')
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+import connectDB from "./config/db.js";
 
-dotenv.config()
+dotenv.config();
 
+connectDB();
 
 const app = express();
 
@@ -20,6 +22,9 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${5000}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${5000}`)
+);
