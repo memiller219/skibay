da22769c7fd6398d622684594cea36454e7f44f3
Michael <memiller219@gmail.com>
2021-05-05 17:01:58 -0700

    added user controllers, middleware, jwt, and user routes

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { authUser, getUserProfile, createUser } from "../controllers/userController.js";
+import { protect } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.post("/login", authUser);
+
+router.route('/profile').get(protect, getUserProfile);
+
+router.route('/').post(createUser);
+
+export default router;
